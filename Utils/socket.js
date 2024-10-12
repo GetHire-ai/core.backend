47f72ab6e540c4a78061028a839a7022f0b10460
Mohammed Owais <filledstackdeveloper@gmail.com>
2024-10-12 17:19:38 +0530

    changes

--- Utils/socket.js
+++ Utils/socket.js
@@ -3,8 +3,9 @@ const Chat = require("../Model/ChatModel");
 const Conversation = require("../Model/ConversationModel");
 const AdminChat = require("../Model/AdminChatModel");
 const AdminConversation = require("../Model/AdminConversationModel");
-
-let onlineUsers = {};
+const CompanyModel = require("../Model/CompanyModel");
+const StudentModel = require("../Model/StudentModel");
+let onlineUsers = [];
 
 module.exports = (server) => {
   const io = socketIo(server, {
@@ -23,20 +24,22 @@ module.exports = (server) => {
 
   io.on("connection", (socket) => {
     socket.on("userConnected", (userId) => {
-      onlineUsers[userId] = socket.id;
-      io.emit("userStatus", { userId, online: true });
-      console.log("conntected", onlineUsers);
+      if (!onlineUsers.some((user) => user.userId === userId)) {
+        onlineUsers.push({ userId, socketId: socket.id });
+        io.emit("userStatus", { userId, online: true });
+      }
+      console.log("connected", onlineUsers);
     });
 
     socket.on("userDisconnected", () => {
-      const userId = Object.keys(onlineUsers).find(
-        (key) => onlineUsers[key] === socket.id
+      const userIndex = onlineUsers.findIndex(
+        (user) => user.socketId === socket.id
       );
-      if (userId) {
-        delete onlineUsers[userId];
-        io.emit("userStatus", { userId, online: false });
+      if (userIndex !== -1) {
+        const [removedUser] = onlineUsers.splice(userIndex, 1);
+        io.emit("userStatus", { userId: removedUser.userId, online: false });
       }
-      console.log("dis-conntected", onlineUsers);
+      console.log("disconnected", onlineUsers);
     });
 
     socket.on("joinConversation", (conversationId) => {
@@ -47,16 +50,48 @@ module.exports = (server) => {
       try {
         const conversations = await Conversation.find({
           participants: userId,
-        }).populate("lastMessage");
-        socket.emit("conversationsList", conversations);
+        }).populate({
+          path: "lastMessage",
+          populate: [{ path: "senderId", model: "Student" }],
+        });
+
+        const updatedConversations = await Promise.all(
+          conversations.map(async (conversation) => {
+            const participantDetails = {
+              company: null,
+              student: null,
+            };
+
+            await Promise.all(
+              conversation?.participants?.map(async (participantId) => {
+                let student = await StudentModel.findById(participantId);
+                if (student) {
+                  participantDetails.student = student;
+                }
+                let company = await CompanyModel.findById(participantId);
+                if (company) {
+                  participantDetails.company = company;
+                }
+              })
+            );
+
+            const conversationCopy = {
+              ...conversation.toObject(),
+              participantDetails,
+            };
+            return conversationCopy;
+          })
+        );
+        socket.emit("conversationsList", updatedConversations);
       } catch (error) {
         console.error("Error fetching conversations:", error);
       }
     });
-    socket.on("getMessages", async (conversationId) => {
+
+    socket.on("getConversationMessages", async (conversationId) => {
       try {
         const messages = await Chat.find({ conversationId });
-        socket.emit("messagesList", messages);
+        socket.emit("conversationMessages", messages);
       } catch (error) {
         console.error("Error fetching messages:", error);
       }
@@ -64,25 +99,23 @@ module.exports = (server) => {
 
     socket.on("sendMessage", async (data) => {
       const { conversationId, senderId, senderType, message } = data;
-      const chatMessage = new Chat({
-        conversationId,
-        senderId,
-        senderType,
-        message,
-      });
+      const chatMessage = new Chat(data);
+
       try {
-        console.log(data);
-        await chatMessage.save();
-        await Conversation.findByIdAndUpdate(conversationId, {
+        const saved = await chatMessage.save();
+        const conversation = await Conversation.findByIdAndUpdate(conversationId, {
           lastMessage: chatMessage._id,
           lastMessageTime: chatMessage.timestamp,
-        });
-        io.to(conversationId).emit("receiveMessage", chatMessage);
-        io.emit("userStatus", { userId: senderId, online: true });
+        }, { new: true });
+        if (conversation && saved) {
+          io.to(conversationId).emit("receiveMessage", chatMessage, conversation);
+          io.emit("userStatus", { userId: senderId, online: true });
+        }
       } catch (error) {
         console.error("Error saving message to database:", error);
       }
     });
+
     socket.on("sendMessageAdmin", async (data) => {
       const { conversationId, senderId, senderType, message } = data;
       const chatMessage = new AdminChat({
@@ -91,6 +124,7 @@ module.exports = (server) => {
         senderType,
         message,
       });
+
       try {
         await chatMessage.save();
         await AdminConversation.findByIdAndUpdate(conversationId, {
@@ -103,6 +137,15 @@ module.exports = (server) => {
       }
     });
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => {
+      const userIndex = onlineUsers.findIndex(
+        (user) => user.socketId === socket.id
+      );
+      if (userIndex !== -1) {
+        const [removedUser] = onlineUsers.splice(userIndex, 1);
+        io.emit("userStatus", { userId: removedUser.userId, online: false });
+      }
+      console.log("disconnected", onlineUsers);
+    });
   });
 };
